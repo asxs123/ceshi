b872fff826b3ea8db34ac51ba931015063cc40da
asxs123 <54346276+asxs123@users.noreply.github.com>
2024-04-26 12:11:27 +0800

    1

--- iStoreOS/diy-part2.sh
+++ iStoreOS/diy-part2.sh
@@ -159,15 +159,15 @@ else
 fi
 rm -rf ${GITHUB_WORKSPACE}/openwrt/{AdGuardHome_${Arch}.tar.gz,AdGuardHome}
 
-echo "预置unblockneteasemusic内核"
-mkdir -p ${GITHUB_WORKSPACE}/openwrt/files/usr/share/unblockneteasemusic
-NAME="${GITHUB_WORKSPACE}/openwrt/files/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
-# echo "" > "$NAME/core_local_ver"
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
+# echo "预置unblockneteasemusic内核"
+# mkdir -p ${GITHUB_WORKSPACE}/openwrt/files/usr/share/unblockneteasemusic
+# NAME="${GITHUB_WORKSPACE}/openwrt/files/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
+# # echo "" > "$NAME/core_local_ver"
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
+# curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
 
 echo "添加默认登录壁纸"
 mkdir -p ${GITHUB_WORKSPACE}/openwrt/files/www/luci-static/argon/background/
